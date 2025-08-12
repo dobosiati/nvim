bbfd2452075d56fe1de4ff239906f9595be32193
Attila Dobosi <attila@dcodegroup.com>
2025-08-12 11:38:55 +0200

    changing the editor theme by the system theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,8 +2,38 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = false,
     priority = 1000,
     config = function()
+      -- Detect GNOME color-scheme
+      local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
+      local result = handle:read("*a") or ""
+      handle:close()
+
+      -- Trim whitespace
+      result = result:gsub("%s+", "")
+
+      -- Choose theme flavor
+      local flavor = "mocha" -- default dark
+      if result == "'default'" then
+        flavor = "latte" -- light mode
+      elseif result == "'prefer-dark'" then
+        flavor = "mocha" -- dark mode
+      end
+
+      require("catppuccin").setup({
+        flavour = flavor,
+        integrations = {
+          telescope = true,
+          treesitter = true,
+          lsp_saga = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = { errors = { "italic" }, hints = { "italic" } },
+          },
+        },
+      })
+
       vim.cmd.colorscheme("catppuccin")
     end,
   },
